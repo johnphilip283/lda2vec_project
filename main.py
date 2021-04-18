1acf833b416184938f4826c4ca1ae1d3db9db528
John Philip <john.philip@buzzfeed.com>
2021-04-18 18:15:09 -0400

    add model file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from preprocess import DataProcessor
 import numpy as np
 from data_utils import load_preprocessed_data
+from model import LDA2Vec
 
 # Data directory
 data_dir ="data"
@@ -19,5 +20,34 @@ processor.preprocess(clean_data_dir)
 
 (idx_to_word, word_to_idx, freqs, pivot_ids, target_ids, doc_ids) = load_preprocessed_data(clean_data_dir)
 
-print(len(word_to_idx))
-print(len(idx_to_word))
+num_docs = len(np.unique(doc_ids))
+vocab_size = len(idx_to_word)
+
+embed_size = 128
+num_topics = 20
+switch_loss_epoch = 5
+
+save_graph = True
+num_epochs = 200
+batch_size = 512
+
+# Initialize the model
+model = LDA2Vec(num_docs,
+          vocab_size,
+          num_topics,
+          embedding_size=embed_size,
+          freqs=freqs,
+          batch_size=batch_size,
+          save_graph_def=save_graph)
+
+# Train the model
+model.train(pivot_ids,
+        target_ids,
+        doc_ids,
+        len(pivot_ids),
+        num_epochs,
+        idx_to_word=idx_to_word,
+        switch_loss_epoch=switch_loss_epoch)
+
+# Visualize topics with pyldavis
+generate_ldavis_data(clean_data_dir, model, idx_to_word, freqs, vocab_size)
---
+++ model.py
@@ -0,0 +1,8 @@
+
+class LDA2Vec:
+  
+  def __init__(self):
+    pass
+
+  def train(self):
+    pass
